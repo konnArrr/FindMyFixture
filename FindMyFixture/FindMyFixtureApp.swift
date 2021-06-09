54e708f7fe1170902151fe160d6daec431ffd9c0
Konne <konnevonronks@gmail.com>
2021-06-09 21:08:29 +0200

    Initial Commit

---
+++ FindMyFixture/FindMyFixtureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyFixtureApp.swift
+//  FindMyFixture
+//
+//  Created by Student on 09.06.21.
+//
+
+import SwiftUI
+
+@main
+struct FindMyFixtureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
